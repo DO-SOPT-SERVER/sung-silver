20d378be27d5dcc3fa4544546af6cfccc5ae3b02
sung-silver <jinna0319@gmail.com>
2023-10-21 23:33:49 +0900

    [INIT] 2주차 세미나 코드 복사

---
+++ [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/dto/request/MemberCreateRequest.java
@@ -0,0 +1,12 @@
+package com.server.dosopt.seminar.dto.request;
+
+import com.server.dosopt.seminar.domain.SOPT;
+import lombok.Data;
+
+@Data
+public class MemberCreateRequest {
+    private String name;
+    private String nickname;
+    private int age;
+    private SOPT sopt;
+}
---
+++ [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/repository/MemberJpaRepository.java
@@ -0,0 +1,12 @@
+package com.server.dosopt.seminar.repository;
+
+import com.server.dosopt.seminar.domain.Member;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+    default Member findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(
+                () -> new EntityNotFoundException("존재하지 않는 회원입니다."));
+    }
+}
