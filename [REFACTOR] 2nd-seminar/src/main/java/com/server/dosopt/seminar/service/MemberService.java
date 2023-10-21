a33ca34e6bfaa13bc5e2fed54db302097f13de2d
sung-silver <jinna0319@gmail.com>
2023-10-22 00:26:22 +0900

    [REFACTOR] DTO 클래스 record 변경에 따른 service 코드 내 get 메서드 호출 방식 수정

--- [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
+++ [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/service/MemberService.java
@@ -36,10 +36,10 @@ public class MemberService {
     @Transactional
     public String create(MemberCreateRequestDTO request) {
         Member member =  memberJpaRepository.save(Member.builder()
-                .name(request.getName())
-                .nickname(request.getNickname())
-                .age(request.getAge())
-                .sopt(request.getSopt())
+                .name(request.name())
+                .nickname(request.nickname())
+                .age(request.age())
+                .sopt(request.sopt())
                 .build());
         return member.getId().toString();
     }
@@ -47,7 +47,7 @@ public class MemberService {
     @Transactional
     public void updateSOPT(Long memberId, MemberProfileUpdateRequestDTO request) {
         Member member = memberJpaRepository.findByIdOrThrow(memberId);
-        member.updateSOPT(new SOPT(request.getGeneration(), request.getPart()));
+        member.updateSOPT(new SOPT(request.generation(), request.part()));
     }
 
     @Transactional
