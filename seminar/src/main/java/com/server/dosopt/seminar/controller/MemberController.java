8cedee8c75196a27dbd93a10249f463a06ed1b02
sung-silver <jinna0319@gmail.com>
2023-10-21 23:20:00 +0900

    [FEAT] 세미나 실습 코드

---
+++ seminar/src/main/java/com/server/dosopt/seminar/controller/MemberController.java
@@ -0,0 +1,58 @@
+package com.server.dosopt.seminar.controller;
+
+import com.server.dosopt.seminar.dto.request.MemberCreateRequest;
+import com.server.dosopt.seminar.dto.request.MemberProfileUpdateRequest;
+import com.server.dosopt.seminar.dto.response.MemberGetResponse;
+import com.server.dosopt.seminar.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberGetResponse> getMemberProfileV1(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberByIdV2(memberId));
+    }
+
+    @GetMapping(value = "/{memberId}/v2", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MemberGetResponse> getMemberProfileV2(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMemberByIdV2(memberId));
+    }
+
+    // 생성
+    @PostMapping
+    public ResponseEntity<Void> createMember(@RequestBody MemberCreateRequest request) {
+        URI location =  URI.create(memberService.create(request));
+        return ResponseEntity.created(location).build();
+    }
+
+    // 목록 조회
+    @GetMapping
+    public ResponseEntity<List<MemberGetResponse>> getMembersProfile() {
+        return ResponseEntity.ok(memberService.getMembers());
+    }
+
+    // 수정
+    @PatchMapping("/{memberId}")
+    public ResponseEntity<Void> updateMemberSoptInfo(@PathVariable Long memberId, @RequestBody MemberProfileUpdateRequest request) {
+        memberService.updateSOPT(memberId, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 삭제
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMember(@PathVariable Long memberId) {
+        memberService.deleteMember(memberId);
+        return ResponseEntity.noContent().build();
+    }
+}
