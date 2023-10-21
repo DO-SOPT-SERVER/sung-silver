b4750e22111a1b89e459881bf8ebe39e23b26865
sung-silver <jinna0319@gmail.com>
2023-10-22 00:10:16 +0900

    [FEAT] SuccessType 추가

--- [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/dto/ErrorType/SuccessType.java
+++ [REFACTOR] 2nd-seminar/src/main/java/com/server/dosopt/seminar/dto/ErrorType/SuccessType.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessType {
-    OK_SUCCESS(HttpStatus.OK, "OK");
+    MEMBER_CREATE_SUCCESS(HttpStatus.CREATED, "사용자 생성에 성공했습니다"),
+    GET_ALL_MEMBER_INFO(HttpStatus.OK, "전체 사용자 조회에 성공했습니다"),
+    GET_MEMBER_INFO(HttpStatus.OK, "사용자 조회에 성공했습니다"),
+    DELETE_MEMBER(HttpStatus.OK, "사용자 삭제에 성공했습니다"),
+    UPDATE_MEMBER(HttpStatus.OK, "사용자 정보 수정에 성공했습니다");
+
     private final HttpStatus status;
     private final String message;
     public int getHttpsStatusCode(){return status.value();}
