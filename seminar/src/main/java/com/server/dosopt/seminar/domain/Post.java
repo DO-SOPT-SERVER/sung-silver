215752432d96a91efa3a33328f7e4c8f7020ee72
sung-silver <jinna0319@gmail.com>
2023-11-06 12:49:36 +0900

    [FEAT] POST 도메인 생성

---
+++ seminar/src/main/java/com/server/dosopt/seminar/domain/Post.java
@@ -0,0 +1,33 @@
+package com.server.dosopt.seminar.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    public Post(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+}
